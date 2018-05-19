4407b38ff9ed3ad1c983436cc9d7b1f4bc7dc8f6
JohnMWood <jmw.home@gmail.com>
2018-05-18 17:43:56 -0700

    Updated Course for testing with list comprehension to generate Course objects

--- course.py
+++ course.py
@@ -1,5 +1,18 @@
 class Course: 
+    """this object will be instanciated for every course found in the user's I-Learn 
+
+    Course will scrap the data and store it for TD 
+    """
     def __init__(self, driver, name, div): 
-        self.name = name 
         self.driver = driver 
+        self.name = name 
         self.div = div 
+
+        # fill dictionary with structured data from course 
+        self.data = {} 
+
+        self.scrap_course_data() 
+
+    def scrap_course_data(self): 
+        pass 
+
